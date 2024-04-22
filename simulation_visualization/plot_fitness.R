fbd0631d3a29781182e40c0aedda18ce9688e715
TSakamoto-evo <71111908+TSakamoto-evo@users.noreply.github.com>
2024-04-22 16:51:05 -0600

    Delete simulation_visualization directory

--- simulation_visualization/plot_fitness.R
+++
@@ -1,6 +0,0 @@
-data <- read.table("regi_fitness.txt")
-
-head(data)
-
-plot(data[,1], data[,2], col="red", type="l", ylim=c(0, 1), xlab="time", ylab="fitness")
-points(data[,1], data[,3], col="blue", type="l")
